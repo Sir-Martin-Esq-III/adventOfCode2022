4882bc05fa68ac7130d1999bf732ae25893021c5
Thomas <thomasmartin98@hotmail.com>
2022-12-01 14:24:21 +0000

    refactor

--- src/day1.rs
+++ src/day1.rs
@@ -1,29 +1,26 @@
 //Bit grim
 #[aoc_generator(day1)]
-pub fn input_generator(input: &str) -> Vec<Vec<i32>> {
+pub fn input_generator(input: &str) -> Vec<i32> {
     return input
         .split("\n\n")
         .map(|sp| {
             return sp
-                .split('\n')
+                .lines()
                 .map(|input| return input.parse::<i32>().unwrap())
-                .collect::<Vec<i32>>();
+                .sum::<i32>();
         })
-        .collect::<Vec<Vec<i32>>>();
+        .collect();
 }
 
 #[aoc(day1, part1)]
-pub fn solve_part1(input: &Vec<Vec<i32>>) -> i32 {
-    input.iter().map(|val| val.iter().sum()).max().unwrap()
+pub fn solve_part1(input: &Vec<i32>) -> i32 {
+    *input.iter().max().unwrap()
 }
 
 #[aoc(day1, part2)]
-pub fn solve_part2(input: &Vec<Vec<i32>>) -> i32 {
-    let mut sums = input
-        .iter()
-        .map(|val| val.iter().sum::<i32>())
-        .collect::<Vec<i32>>();
-
-    sums.sort();
-    sums.iter().rev().take(3).sum()
+pub fn solve_part2(input: &Vec<i32>) -> i32 {
+    //You can't pass in a &mut as an arg to the function so you need to clone it :(
+    let mut t = input.clone();
+    t.sort_by(|a, b| b.cmp(a));
+    t.iter().take(3).sum()
 }
