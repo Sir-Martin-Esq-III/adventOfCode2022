d78d11793f54348deb6c454ad9845433c5e0111b
Thomas <thomasmartin98@hotmail.com>
2022-12-08 18:33:36 +0000

    refactor pt1

--- src/day8.rs
+++ src/day8.rs
@@ -1,3 +1,6 @@
+use core::panic;
+use std::ops::Range;
+
 use itertools::{rev, Itertools};
 
 #[aoc_generator(day8)]
@@ -8,55 +11,63 @@ pub fn input_generator(input: &str) -> Vec<Vec<u32>> {
         .collect_vec()
 }
 
-fn check_left(board: &Vec<Vec<u32>>, pos: (usize, usize)) -> bool {
-    let tree_height = board[pos.1][pos.0];
-
-    for i in (0..pos.0).rev() {
-        if board[pos.1][i] >= tree_height {
-            return false;
-        }
-    }
-
-    return true;
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
 }
 
-fn check_right(board: &Vec<Vec<u32>>, pos: (usize, usize), lim: usize) -> bool {
-    let tree_height = board[pos.1][pos.0];
-    for i in pos.0 + 1..lim {
-        if board[pos.1][i] >= tree_height {
-            return false;
+fn check_direction(
+    board: &[Vec<u32>],
+    pos: (usize, usize),
+    direction: Direction,
+    bound: Option<usize>,
+) -> bool {
+    let current_tree_height = board[pos.1][pos.0];
+    let bnd = bound.unwrap_or(0 as usize);
+
+    match direction {
+        Direction::Up => {
+            for i in pos.1 + 1..bnd {
+                if board[i][pos.0] >= current_tree_height {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        Direction::Down => {
+            for i in (bnd..pos.1).rev() {
+                if board[i][pos.0] >= current_tree_height {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        Direction::Left => {
+            for i in (bnd..pos.0).rev() {
+                if board[pos.1][i] >= current_tree_height {
+                    return false;
+                }
+            }
+            return true;
+        }
+        Direction::Right => {
+            for i in pos.0 + 1..bnd {
+                if board[pos.1][i] >= current_tree_height {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
-
-    return true;
-}
-
-fn check_up(board: &Vec<Vec<u32>>, pos: (usize, usize)) -> bool {
-    let tree_height = board[pos.1][pos.0];
-
-    for i in (0..pos.1).rev() {
-        if board[i][pos.0] >= tree_height {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-fn check_down(board: &Vec<Vec<u32>>, pos: (usize, usize), lim: usize) -> bool {
-    let tree_height = board[pos.1][pos.0];
-
-    for i in pos.1 + 1..lim {
-        if board[i][pos.0] >= tree_height {
-            return false;
-        }
-    }
-
-    return true;
 }
 
 #[aoc(day8, part1)]
-pub fn solve_part1(input: &Vec<Vec<u32>>) -> i32 {
+pub fn solve_part1(input: &[Vec<u32>]) -> i32 {
     let mut sum: i32 = 0;
 
     let width = input[0].len();
@@ -64,12 +75,11 @@ pub fn solve_part1(input: &Vec<Vec<u32>>) -> i32 {
 
     for i in 1..height - 1 {
         for j in 1..width - 1 {
-            let g = check_left(input, (j, i))
-                || check_right(input, (j, i), width)
-                || check_up(input, (j, i))
-                || check_down(input, (j, i), height);
-
-            if (g) {
+            if check_direction(input, (j, i), Direction::Left, None)
+                || check_direction(input, (j, i), Direction::Right, Some(width))
+                || check_direction(input, (j, i), Direction::Up, None)
+                || check_direction(input, (j, i), Direction::Down, Some(height))
+            {
                 sum += 1
             }
         }
@@ -78,7 +88,7 @@ pub fn solve_part1(input: &Vec<Vec<u32>>) -> i32 {
     sum + 2 * width as i32 + 2 * height as i32 - 4
 }
 
-fn check_left_num(board: &Vec<Vec<u32>>, pos: (usize, usize)) -> i32 {
+fn check_left_num(board: &[Vec<u32>], pos: (usize, usize)) -> i32 {
     let tree_height = board[pos.1][pos.0];
     let mut sum = 0;
 
@@ -92,7 +102,7 @@ fn check_left_num(board: &Vec<Vec<u32>>, pos: (usize, usize)) -> i32 {
     return sum;
 }
 
-fn check_right_num(board: &Vec<Vec<u32>>, pos: (usize, usize), lim: usize) -> i32 {
+fn check_right_num(board: &[Vec<u32>], pos: (usize, usize), lim: usize) -> i32 {
     let tree_height = board[pos.1][pos.0];
     let mut sum = 0;
     for i in pos.0 + 1..lim {
@@ -105,7 +115,7 @@ fn check_right_num(board: &Vec<Vec<u32>>, pos: (usize, usize), lim: usize) -> i3
     return sum;
 }
 
-fn check_up_num(board: &Vec<Vec<u32>>, pos: (usize, usize)) -> i32 {
+fn check_up_num(board: &[Vec<u32>], pos: (usize, usize)) -> i32 {
     let tree_height = board[pos.1][pos.0];
     let mut sum = 0;
     for i in (0..pos.1).rev() {
@@ -118,7 +128,7 @@ fn check_up_num(board: &Vec<Vec<u32>>, pos: (usize, usize)) -> i32 {
     return sum;
 }
 
-fn check_down_num(board: &Vec<Vec<u32>>, pos: (usize, usize), lim: usize) -> i32 {
+fn check_down_num(board: &[Vec<u32>], pos: (usize, usize), lim: usize) -> i32 {
     let tree_height = board[pos.1][pos.0];
     let mut sum = 0;
     for i in pos.1 + 1..lim {
@@ -132,9 +142,7 @@ fn check_down_num(board: &Vec<Vec<u32>>, pos: (usize, usize), lim: usize) -> i32
 }
 
 #[aoc(day8, part2)]
-pub fn solve_part2(input: &Vec<Vec<u32>>) -> i32 {
-    let mut sum: i32 = 0;
-
+pub fn solve_part2(input: &[Vec<u32>]) -> i32 {
     let width = input[0].len();
     let height = input.len();
 
