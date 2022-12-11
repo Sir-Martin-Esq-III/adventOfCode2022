a0bc97adb438ec1fbb1e757bc486a5df89457c83
Thomas <thomasmartin98@hotmail.com>
2022-12-11 20:49:28 +0000

    clean up day 2

--- src/day11.rs
+++ src/day11.rs
@@ -1,3 +1,4 @@
+use std::collections::BTreeSet;
 use std::ops::AddAssign;
 use std::{collections::VecDeque, fmt::Debug};
 
@@ -90,78 +91,10 @@ fn unoffical_generate_input_because_i_cant_bloody_use_mut_ref_as_an_input_to_the
         .collect_vec();
 }
 
-// fn run_rounds(monkey_list: &mut Vec<Monkey>, rounds: i32) {
-//     let common_multiple: u64 = monkey_list.iter().map(|monkey| monkey.test_div).product();
-
-//     for i in 0..rounds {
-//         for j in 0..monkey_list.len() {
-//             for _ in 0..monkey_list[j].items.len() {
-//                 let new_worry: u64 = (monkey_list[j].operation)(&monkey_list[j].items[0]);
-
-//                 monkey_list[j].inspected_counter += 1;
-
-//                 let new_monkey_index: usize;
-
-//                 let new_worry = new_worry % common_multiple;
-
-//                 if &new_worry % monkey_list[j].test_div as u64 == 0 {
-//                     new_monkey_index = monkey_list[j].monkey_idx[0] as usize;
-//                 } else {
-//                     new_monkey_index = monkey_list[j].monkey_idx[1] as usize;
-//                 }
-
-//                 monkey_list[new_monkey_index].items.push_back(new_worry);
-
-//                 monkey_list[j].items.pop_front();
-//             }
-//         }
-//     }
-// }
-
-// #[aoc(day11, part1)]
-// pub fn solve_part1(input: &str) -> i32 {
-//     let rounds = 20;
-//     let mut monkey_list = unoffical_generate_input_because_i_cant_bloody_use_mut_ref_as_an_input_to_the_solver_functions(input);
-
-//     for _ in 0..rounds {
-//         for j in 0..monkey_list.len() {
-//             for _ in 0..monkey_list[j].items.len() {
-//                 let new_worry: u32 = ((monkey_list[j].operation)(&monkey_list[j].items[0]) / 3)
-//                     .try_into()
-//                     .unwrap();
-
-//                 monkey_list[j].inspected_counter += 1;
-
-//                 let new_monkey_index: usize;
-
-//                 if new_worry % monkey_list[j].test_div == 0 {
-//                     new_monkey_index = monkey_list[j].monkey_idx[0] as usize;
-//                 } else {
-//                     new_monkey_index = monkey_list[j].monkey_idx[1] as usize;
-//                 }
-
-//                 monkey_list[new_monkey_index]
-//                     .items
-//                     .push_back(new_worry.try_into().unwrap());
-
-//                 monkey_list[j].items.pop_front();
-//             }
-//         }
-//     }
-
-//     println!("{:#?}", monkey_list);
-//     1
-// }
-
-#[aoc(day11, part2)]
-pub fn solve_part2(input: &str) -> i32 {
-    let rounds = 10000;
-    let mut monkey_list = unoffical_generate_input_because_i_cant_bloody_use_mut_ref_as_an_input_to_the_solver_functions(input);
-
+fn run_rounds(monkey_list: &mut Vec<Monkey>, rounds: i32) -> u64 {
     let common_multiple: u64 = monkey_list.iter().map(|monkey| monkey.test_div).product();
 
-    for i in 0..rounds {
-        // println!("round: {:?}", i);
+    for _ in 0..rounds {
         for j in 0..monkey_list.len() {
             for _ in 0..monkey_list[j].items.len() {
                 let new_worry: u64 = (monkey_list[j].operation)(&monkey_list[j].items[0]);
@@ -185,6 +118,27 @@ pub fn solve_part2(input: &str) -> i32 {
         }
     }
 
-    println!("{:#?}", monkey_list);
-    1
+    let mut sorted_list = monkey_list
+        .iter()
+        .map(|monkey| return monkey.inspected_counter)
+        .collect_vec();
+
+    sorted_list.sort();
+    println!("{:?}", sorted_list);
+
+    sorted_list[6] * sorted_list[7]
+}
+
+#[aoc(day11, part1)]
+pub fn solve_part1(input: &str) -> u64 {
+    let mut monkey_list = unoffical_generate_input_because_i_cant_bloody_use_mut_ref_as_an_input_to_the_solver_functions(input);
+
+    run_rounds(&mut monkey_list, 20)
+}
+
+#[aoc(day11, part2)]
+pub fn solve_part2(input: &str) -> u64 {
+    let mut monkey_list = unoffical_generate_input_because_i_cant_bloody_use_mut_ref_as_an_input_to_the_solver_functions(input);
+
+    run_rounds(&mut monkey_list, 10000)
 }
